6694c26521a90ed16829d3c0b2cd296aad45d9ca
RChiang8 <chiang.ryan@hotmail.com>
2016-01-23 18:15:48 -0800

    Made my rocket!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+
+
+//hey this is invisible comment
+/*
+ajdfahdlfajd
+ajdfla;
+ajl;sd
+*/
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+	// changing class name
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
 var sayMyName= function (name) {
 	alert('My name is: ' +name);
 }
